2409d9dcacf72bdc4ab04b84d2ab163b891178b0
Ivan Nikolić <niksy5@gmail.com>
2015-11-08 08:34:39 +0100

    Improve CLI interface

--- cli.js
+++ cli.js
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
 
-require('./');
+var index = require('./');
+var parse = require('./lib/parse');
+
+index()
+	.then(function ( items ) {
+		var count = items.length;
+		parse.clearReadingList();
+		console.log('Saved ' + count + ' ' + (count === 1 ? 'link' : 'links') + '.');
+	})
+	.catch(function ( err ) {
+		console.error('Error ocurred: ' + err);
+	});
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var parse = require('./lib/parse');
 var pinboard = require('./lib/pinboard');
 
-parse()
-	.then(function ( items ) {
-		return pinboard(items);
-	})
-	.then(function ( items ) {
-		var count = items.length;
-		console.log('Saved ' + count + ' ' + (count === 1 ? 'link' : 'links') + '.');
-	});
+/**
+ * @return {Promise}
+ */
+module.exports = function () {
+
+	return parse()
+		.then(function ( items ) {
+			return pinboard(items);
+		});
+
+};
